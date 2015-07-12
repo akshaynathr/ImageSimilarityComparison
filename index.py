9d04dcd89bce1959895e5998ef93cedcf3d6b0ab
akshaynathr <akshaynathr@gmail.com>
2015-07-12 11:29:43 +0530

    Histogram comparison files

---
+++ index.py
@@ -0,0 +1,37 @@
+from imgSearch import descriptor
+import glob
+import argparse
+
+import cv2
+
+#ap=argparse.ArgumentParser()
+#ap.add_argument("-d","--dataset",required=True, help="Path to the directory that contains images to be indexed")
+
+#ap.add_argument("-i","--index",required=True,help = "Path to computed index stored")
+
+#args=vars(ap.parse_args())
+
+def index(Dataset):
+    indexfile='index.csv'
+
+    dataset=Dataset
+
+    cd=descriptor((8,12,3))
+
+    output=open(indexfile,"w")
+
+
+    for imagePath in glob.glob(dataset+"*.jpg"):
+        imageID=imagePath[imagePath.rfind("/")+1:]
+        image=cv2.imread(imagePath)
+
+
+        features=cd.describe(image)
+
+        features=[str(f) for f in features]
+
+        output.write("%s,%s\n" % (imageID,",".join(features) ) )
+
+
+    output.close()
+
---
+++ search.py
@@ -0,0 +1,75 @@
+from imgSearch import descriptor
+import math
+from searcher import Searcher
+import os
+import argparse
+
+import cv2
+
+
+def matcher(Dataset,Query):
+    dataset='uploads'
+
+    query_image=Query
+    
+    index='index.csv'
+    
+    res=0
+    res1=0
+
+    cd=descriptor((8,12,3))
+
+    query=cv2.imread(query_image)
+
+    gray=cv2.cvtColor(query,cv2.COLOR_BGR2GRAY)
+    cv2.imwrite('grey.jpg',gray)
+
+    query_grey=cv2.imread('grey.jpg')
+
+
+    features=cd.describe(query)
+
+    features_grey=cd.describe(query_grey)
+
+    searcher=Searcher(index)
+    results=searcher.search(features)
+    results_grey=searcher.search(features_grey)
+
+
+    #cv2.imshow("Query",query)
+
+    for (score,resultID) in results:
+        result=cv2.imread(dataset+resultID)
+        #cv2.imshow("Result",result)
+        print(resultID)
+        if score < 7.95:
+            print(score)
+            t=math.floor(score)
+            res=100-t
+            print(100-t)
+        else:
+            print(score)
+            t=math.floor(score)
+            res=t
+            print(t+t)
+            #cv2.waitKey(0)
+
+
+    print("---------------grey---------------")
+    for (score,resultID) in results_grey:
+        result=cv2.imread(dataset+resultID)
+        #cv2.imshow("Result",result)
+        print(resultID)
+        if score < 7.95:
+            print(score)
+            t=math.floor(score)
+            res1=100-t
+            print(100-t)
+        else:
+            print(score)
+            t=math.floor(score)
+            res1=t
+            print(t+t)
+            #cv2.waitKey(0)
+    return (res,res1) 
+
