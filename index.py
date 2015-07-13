6f3a1fe5ef51b8b141a639b9a4d1ae4bfbadd3e0
akshaynathr <akshaynathr@gmail.com>
2015-07-13 23:50:43 +0530

    added support for png

--- index.py
+++ index.py
@@ -33,5 +33,18 @@ def index(Dataset):
         output.write("%s,%s\n" % (imageID,",".join(features) ) )
 
 
+
+    for imagePath in glob.glob(dataset+"*.png"):
+        imageID=imagePath[imagePath.rfind("/")+1:]
+        image=cv2.imread(imagePath)
+
+
+        features=cd.describe(image)
+
+        features=[str(f) for f in features]
+
+        output.write("%s,%s\n" % (imageID,",".join(features) ) )
+
+
     output.close()
 
